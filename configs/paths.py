fd7b2d37c67d0fb967bbcd6721dc3e573a1b9462
Max Dumas <max@dumas.nyc>
2023-02-12 21:25:05 +0000

    Use portable data paths

--- configs/paths.py
+++ configs/paths.py
@@ -1,20 +1,26 @@
-import socket
-import getpass
+# import socket
+# import getpass
 
-username = getpass.getuser()
+# username = getpass.getuser()
 
-hostname = socket.gethostname()
+# hostname = socket.gethostname()
+import os
 
-if hostname == 'yenchi-pc':
-    dataroot = '/home/yenchi/generative_transformers/data'
-    logroot = '/home/yenchi/generative_transformers/logs'
-elif hostname == 'u110459':
-    dataroot= '/data/datasets/paritosh'
-    logroot= '/data/paritosh/generative_transformers/logs'
-    if username == 'yenchi':
-        dataroot = '/data/yenchi/generative_transformers/data'
-        logroot = '/data/yenchi/generative_transformers/logs'
-elif hostname == 'euclid':
-    if username == 'yenchi':
-        dataroot = '/data/yenchi/generative_transformers/data'
-        logroot = '/data/yenchi/generative_transformers/logs'
+current_dir = os.getcwd()
+
+dataroot = os.path.join(current_dir, "data")
+logroot = os.path.join(current_dir, "logs")
+
+# if hostname == 'yenchi-pc':
+#     dataroot = '/home/yenchi/generative_transformers/data'
+#     logroot = '/home/yenchi/generative_transformers/logs'
+# elif hostname == 'u110459':
+#     dataroot= '/data/datasets/paritosh'
+#     logroot= '/data/paritosh/generative_transformers/logs'
+#     if username == 'yenchi':
+#         dataroot = '/data/yenchi/generative_transformers/data'
+#         logroot = '/data/yenchi/generative_transformers/logs'
+# elif hostname == 'euclid':
+#     if username == 'yenchi':
+#         dataroot = '/data/yenchi/generative_transformers/data'
+#         logroot = '/data/yenchi/generative_transformers/logs'
